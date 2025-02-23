db36b8e7dd03b9db8a82dd1dde71e1f8f457d43f
avierkin03 <93377331+avierkin03@users.noreply.github.com>
2025-02-23 19:29:37 +0200

    Update sits.py

--- sits.py
+++ sits.py
@@ -4,7 +4,11 @@ from kivy.clock import Clock
 # Клас для створення віджета, який показуватиме к-ть присідань які залишилося зробити
 class Sits(Label):
     def __init__(self, total, **kwargs):
-        pass
+        self.total = total
+        self.current = 0
+        super().__init__(text = f"Залишилося присідань: {self.total}")
 
+    # Метод, який буде спрацьовувати після кожного виконання анімації бігунком
     def next(self, *args):
-        pass
+        self.current += 1
+        self.text = f"Залишилося присідань: {self.total - self.current}"
